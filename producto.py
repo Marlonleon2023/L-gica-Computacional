03ef5b8f6628cc06539391771ad3417384df62dc
Marlon esteban leon <146051758+Marlonleon2023@users.noreply.github.com>
2023-11-24 14:14:29 -0500

    Creacion de interfaz.py

---
+++ producto.py
@@ -0,0 +1,201 @@
+
+
+camisa = 100
+pantalon = 250
+zapatos = 500
+
+
+
+##camisa = float(input('cuantas camisas quieres: '))
+##camisa=100 *camisa *1 
+##print('compraste camisa  {}'.format(camisa))
+
+##zapatos = float(input('cuantas zapatos quieres: '))
+##zapatos=500 *zapatos *1 
+##print('compraste zapatos  {}'.format(zapatos))
+
+##pantalon = float(input('cuantos pantalones quieres: '))
+##pantalon=250 *pantalon *1 
+##print('compraste pantalon  {}'.format(pantalon))
+
+##print(camisa + pantalon + zapatos)
+
+sueldosemana = 1.856016
+horas = 8
+valordeldía= 38.667
+pagopordía =309.336
+díasdetrabajo= 6
+
+
+## sueldo del dia
+valordeldía=38.667*8
+##print(valordeldía)
+pagopordía=309.336
+
+##sueldo de la semana dias (6) pado por dias (309.366)
+pagopordía= 309.336*6
+##print(pagopordía)
+sueldosemana=1.856016
+##pagopordía = float(input('Escriba el numero de dias: '))
+
+##pagopordía=309.336 * pagopordía *1
+
+##print('El sueldo de la semana es {}'.format(pagopordía))
+
+## area del circulo
+from fractions import Fraction
+
+r = float(input('ingresa el radio de tu  circulo: '))
+v = float(input('ingresa nuevo el numero: '))
+
+volumen= 4/3*3.1416*r**3
+
+print('El volumen del circulo es {}'.format(volumen))
+
+##nombre usuario
+##Name= (input('ingresa  tu nombre: '))   
+#Name = ("hola como estas "+ Name)
+#print(Name)
+
+##suma y resta 
+
+
+##NumA=int(input('number1: '))
+##NumB=int(input('number2: '))
+##NumC=int(input('number3: '))
+##suma=NumA+NumC+NumB
+##resta =NumA-NumC-NumB
+##multi=NumA*NumB*NumC
+##print(multi)
+
+
+6 ##promedio de notas 
+
+##perio1=int(input('notafr1: '))
+##perio2=int(input('notaf2: '))
+##perio3=int(input('notaf3: '))
+##perio4=int(input('notaf4: '))
+
+##calcula = perio1+perio2+perio3+perio4
+##calcula =calcula/4
+
+##print(calcula)
+
+##7 convertir a centimetros  a pulgadas
+
+
+##pulgada=0.39370
+##cm=2,54
+
+
+
+##cm=float(input('cm: '))
+
+##print(pulgada*cm)
+##print("pulgadas")
+
+##8 los lados de un triangulo
+
+##print("ingrese los lados del triangulo")
+
+
+                
+##Atri=float(input('alado: '))
+##Btri=float(input('blado: '))
+##Ctri=float(input('clado: '))
+##valor1= Atri-Btri
+##valor2= Atri-Ctri
+
+
+##if valor1 ==0:
+    ##print("no es valido")
+##if valor2 ==0:
+    ##print("no invalido")
+##if valor1 >0:
+    ##print("triangulo escaleno")
+##if valor1<0:
+    ##print("triangulo isoseles")
+##if Atri==Btri==Ctri :
+   ## print("angulo equilatero")
+   
+ #9 calcular si la division es exacta
+##print("division")
+##Dividendo=int(input('dividen: '))
+##Divisor=int(input('divisor: '))
+
+##conciente=Dividendo/Divisor
+
+##print("conciente")
+##print(conciente)
+##print("resto")
+##resto=Dividendo//Divisor
+##resto=Dividendo%Divisor
+##if resto ==0:
+   ## print("es exacta")
+##if resto >0:
+   ## print("no es exacta")
+##print(resto)
+
+
+#10 teorema de pitagoras
+
+##cateto1=int(input('catA: '))
+##cateto2=int(input('catB: '))
+
+
+
+
+
+##cateto3=(cateto1**2)+(cateto2**2)
+
+##print("cateto3")
+##print(cateto3)
+
+##cateto3=cateto1**2+cateto2**2
+##print("hipotenusa")
+##hiptenusa=cateto3
+##if hiptenusa>20 :
+    ##print("elevar **0.5")
+##hiptenusa=int(input('elevar: '))  
+##print(hiptenusa)
+##elevar=hiptenusa**0.5
+##print(elevar)
+
+
+                
+   
+
+               
+               
+               
+                
+                
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
